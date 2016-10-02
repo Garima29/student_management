6a439de2dcd08553316a366fb01b4cb333752497
Garima29 <dadheechgarima371@gmail.com>
2016-10-02 19:15:46 +0530

    Modified no of students

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,7 +6,27 @@ class Student < ActiveRecord::Base
   validates_presence_of :name, :father_name, :mother_name, :phone_no, :city, :state, :classroom_id, :school_id
   validates_numericality_of :phone_no, :school_id, :classroom_id
   validates_length_of :phone_no, is:10
+  before_save :highlight_changes
+  after_create :increase_no_of_students
+  after_update :descrease_no_of_students
   def teachers_id
     self.teacher_ids
+  end
+
+  def increase_no_of_students
+    p "inside increase"
+    @classroom=self.classroom
+    @classroom.update_attributes(:no_of_students=>(@classroom.no_of_students+1))
+  end
+  def descrease_no_of_students
+    p "inside increase"
+    if(@changes.keys.include?('archive'))
+      @classroom=self.classroom
+      @classroom.update_attributes(:no_of_students=>(@classroom.no_of_students-1))
+    end
+  end
+
+  def highlight_changes
+    @changes = self.changes
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe Student, type: :model do
   it { should validate_presence_of(:phone_no) }
   it { should validate_numericality_of(:phone_no) }
   it { should validate_length_of(:phone_no).is_equal_to(10)}
+  context "update_no_of_students" do
+    it "when student is created" do
+      school=FactoryGirl.create(:school)
+      classroom=FactoryGirl.create(:classroom,:school_id=>school.id)
+      teacher=FactoryGirl.create(:teacher,:school_id=>school.id)
+      student1=FactoryGirl.create(:student,:school_id=>school.id,:classroom_id=>classroom.id)
+      student2=FactoryGirl.create(:student,:school_id=>school.id,:classroom_id=>classroom.id)
+      student3=FactoryGirl.create(:student,:school_id=>school.id,:classroom_id=>classroom.id)
+      student3.classroom.no_of_students.should be 4
+    end
+  end
+  context "decrease_no_of_students" do
+    it "when student is removed" do
+      school=FactoryGirl.create(:school)
+      classroom=FactoryGirl.create(:classroom,:school_id=>school.id)
+      teacher=FactoryGirl.create(:teacher,:school_id=>school.id)
+      student1=FactoryGirl.create(:student,:school_id=>school.id,:classroom_id=>classroom.id)
+      student2=FactoryGirl.create(:student,:school_id=>school.id,:classroom_id=>classroom.id)
+      student2.update_attributes(:archive=>true)
+      student2.classroom.no_of_students.should be 2
+    end
+  end
 end
